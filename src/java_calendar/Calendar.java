ba61b0738f9636ba999527c578f6e533223d1b29
SH125B <109638154+SH125B@users.noreply.github.com>
2023-03-09 12:52:52 +0900

    my solution last day of months

--- src/java_calendar/Calendar.java
+++ src/java_calendar/Calendar.java
@@ -1,5 +1,7 @@
 package java_calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -10,6 +12,26 @@ public class Calendar {
 		System.out.println("8   9   10  11  12  13  14");
 		System.out.println("15 16   17  18  19  20  21");
 		System.out.println("22 23   24  25  26  27  28");
+		System.out.println("------------------------------");
+
+		System.out.println("1월부터 12월까지 최대일수를 출력합니다.");
+		System.out.println("1부터 12까지 숫자중 하나를 입력하십시오.");
+		Scanner scanner = new Scanner(System.in);
+		int inputValue = scanner.nextInt();
+		
+		if (inputValue%2 != 0 || inputValue == 8) {
+			System.out.printf("%d월의 최대일수는 %d일입니다.%n", inputValue, 31);
+		} 
+		else if(inputValue == 2){
+			System.out.printf("%d월의 최대일수는 %d일입니다.%n", inputValue, 28);
+		}
+		else {
+			System.out.printf("%d월의 최대일수는 %d일입니다.%n", inputValue, 30);
+		}
+		
+		
+		
+		scanner.close();
 		
 	}
 
