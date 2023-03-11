0455a55237589354dc2aabf92878c49cda4edb52
SH125B <109638154+SH125B@users.noreply.github.com>
2023-03-11 10:12:28 +0900

    separate class, print month-calendar by using printf

--- src/java_calendar/Calendar.java
+++ src/java_calendar/Calendar.java
@@ -1,69 +1,39 @@
 package java_calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 	
 	public static int[] lastdayOfMonths = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public static int getLastdayOfMonths(int a) {
-		return lastdayOfMonths[a-1];
+	public int getLastdayOfMonths(int month) {
+		return lastdayOfMonths[month-1];
 	}
 
-	
-	
-	public static String[] calendarMonths = {"일   월   화   수  목   금   토", "------------------------------"
-,"1   2    3   4   5   6   7", "8   9   10  11  12  13  14", "15 16   17  18  19  20  21"
-, "22 23   24  25  26  27  28", "29 30", "29 30   31", "------------------------------"
-	};
+	public void printCalendar(int year, int month) {
+		
+		System.out.printf("     <%d년%3d월> \n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("----------------------");
 
-	public static void main(String[] args) {
-
-		String prom = "Month> ";
-		System.out.println("1월부터 12월까지 최대일수를 출력합니다.");
-		System.out.println("출력횟수를 입력하시오.");
 		
-		Scanner scanner = new Scanner(System.in);
-		int times = scanner.nextInt();
-		
-		
-		for(int i=0; i<times;i++) {
-			System.out.println("출력을 원하는 달을 숫자로 입력하시오. 중간에 종료하려면 -1를 입력하십시오.");
-			System.out.print(prom);
-			int inputValue = scanner.nextInt();
-			
-			if (inputValue == -1) {
-				break;
-			} 
-			else {
-			System.out.printf("%d월의 마지막날은 %d일입니다.%n", inputValue, getLastdayOfMonths(inputValue));
-			System.out.println("-----------------------------");
-			}
-			
-			if (getLastdayOfMonths(inputValue) == 31) {
-				for(int j=0; j<6; j++) {
-					System.out.println(calendarMonths[j]);
-				}
-				System.out.println(calendarMonths[7]);
-				System.out.println(calendarMonths[8]);
-			}
-			else if (getLastdayOfMonths(inputValue) == 30) {
-				for(int j=0; j<7; j++) {
-					System.out.println(calendarMonths[j]);
-				}
-				System.out.println(calendarMonths[8]);
-			}
-			
-			else {
-				for(int j=0; j<6; j++) {
-					System.out.println(calendarMonths[j]);
-				}
-				System.out.println(calendarMonths[8]);
+		int lastDay = getLastdayOfMonths(month);
+		for (int i=1; i<= lastDay; i++) {
+			System.out.printf("%3d", i);
+			if (i%7 ==0) {
+				System.out.println();
 			}
 		}
-		System.out.println("출력을 종료합니다.");
-		scanner.close();
+		System.out.println();
 		
 	}
+	
+	
+	
+	
+	/* 내 풀이
+	public  String[] calendarMonths = {"일   월   화   수  목   금   토", "------------------------------"
+,"1   2    3   4   5   6   7", "8   9   10  11  12  13  14", "15 16   17  18  19  20  21"
+, "22 23   24  25  26  27  28", "29 30", "29 30   31", "------------------------------"
+	}; */ 
+
 
 }
---
+++ src/java_calendar/Prompt.java
@@ -0,0 +1,72 @@
+package java_calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+	
+	private final static String PROMPT = "Month> ";
+	
+	public void runPrompt() {
+		Calendar cal = new Calendar();
+		System.out.println("1월부터 12월까지 최대일수를 출력합니다.");
+		System.out.println("출력횟수를 입력하시오.");
+		
+		Scanner scanner = new Scanner(System.in);
+		int times = scanner.nextInt();
+		
+		
+		for(int i=0; i<times;i++) {
+			System.out.println("출력을 원하는 달을 숫자로 입력하시오. 중간에 종료하려면 -1를 입력하십시오.");
+			System.out.print(PROMPT);
+			int inputValue = scanner.nextInt();
+			
+			if (inputValue == -1) {
+				break;
+			} 
+			else {
+			System.out.printf("%d월의 마지막날은 %d일입니다.%n", inputValue, cal.getLastdayOfMonths(inputValue));
+			System.out.println("-----------------------------");
+			}
+			
+		cal.printCalendar(2023, inputValue);	
+			
+			
+			
+			
+			
+			/* 내 풀이
+			if (cal.getLastdayOfMonths(inputValue) == 31) {
+				for(int j=0; j<6; j++) {
+					System.out.println(cal.calendarMonths[j]);
+				}
+				System.out.println(cal.calendarMonths[7]);
+				System.out.println(cal.calendarMonths[8]);
+			}
+			else if (cal.getLastdayOfMonths(inputValue) == 30) {
+				for(int j=0; j<7; j++) {
+					System.out.println(cal.calendarMonths[j]);
+				}
+				System.out.println(cal.calendarMonths[8]);
+			}
+			
+			else {
+				for(int j=0; j<6; j++) {
+					System.out.println(cal.calendarMonths[j]);
+				}
+				System.out.println(cal.calendarMonths[8]);
+			} */
+		}
+		System.out.println("출력을 종료합니다.");
+		scanner.close();
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Prompt p = new Prompt();
+		p.runPrompt();
+	}
+
+}
